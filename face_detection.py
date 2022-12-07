5545142d9869afda4a9b831560ac14fda3dcbc04
Eric <eoulster@gmail.com>
2022-12-07 15:30:48 -0500

    Added attention algorithm

--- face_detection.py
+++ face_detection.py
@@ -3,6 +3,8 @@ import mediapipe as mp
 import numpy as np
 import time
 from mss import mss
+from scipy.spatial.distance import cdist
+
 
 mp_face_detection = mp.solutions.face_detection
 
@@ -23,6 +25,27 @@ sct = mss()
     # RIGHT_EAR_TRAGION
     # RIGHT_EYE
 
+def calc_distance(p1, p2):
+    """
+    Basic Euclidean Distance Calculation, while changing the data-types of input.
+    """
+    x1 = float(p1.x)
+    x2 = float(p2.x)
+    y1 = float(p1.y)
+    y2 = float(p2.y)
+    return np.sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
+
+
+def calc_attention(eye, left, right):
+    side_gaps = min(left, right)
+    # if the ear-eye value is smaller than this fraction, you can assume someone is looking to the side.
+    # This value was dialed in manually.
+    eye_gap_threshold = eye / 2
+
+    if side_gaps > eye_gap_threshold:
+        return True
+    else:
+        return False
 
 while True:
 
@@ -38,9 +61,7 @@ while True:
     # Convert the color space from RGB to BGR
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    img_h, img_w, img_c = image.shape
-    face_3d = []
-    face_2d = []
+
     detection_list = []
     if results.detections:
         for detection in results.detections:
@@ -49,16 +70,44 @@ while True:
     #You have to get a count of total number of face objects
     # You have to calculate a total 
     
-    total_faces = len(detection_list)
+    #total_faces = len(detection_list)
+    #person_id = 0
 
-    
+    people_list = []
 
     if len(detection_list) > 0:
         for i in detection_list:
-            foo = mp_face_detection.get_key_point(
-          i, mp_face_detection.FaceKeyPoint.NOSE_TIP)
+            l_ear = mp_face_detection.get_key_point(
+            i, mp_face_detection.FaceKeyPoint.LEFT_EAR_TRAGION)
 
+            l_eye= mp_face_detection.get_key_point(
+            i, mp_face_detection.FaceKeyPoint.LEFT_EYE)
+
+            r_eye= mp_face_detection.get_key_point(
+            i, mp_face_detection.FaceKeyPoint.RIGHT_EYE)
     
+            r_ear= mp_face_detection.get_key_point(
+            i, mp_face_detection.FaceKeyPoint.RIGHT_EAR_TRAGION)
+            
+            eye_gap = calc_distance(l_eye, r_eye)
+            left_gap = calc_distance(l_ear, l_eye)
+            right_gap = calc_distance(r_ear, r_eye)
+            # You can use the X & Y  of the bounding box below to detect the bounding box of each person 
+            # This can single indivduals out for not paying attention
+            # relative_location = i.location_data.relative_bounding_box
+
+            is_attentive = calc_attention(eye_gap, left_gap, right_gap)
+            # If you want an index for singling out people, use a dict
+            # people_list.append({'person_id': person_id, 'is_attentive': is_attentive})
+
+            #Otherwise, use a list:
+            people_list.append(is_attentive)
+            # Increment value for next person in loop
+            # person_id += 1
+
+    paying_attention = sum(people_list)
+    
+    total = len(people_list)
 
     cv2.imshow('Face Detection', image)
 
